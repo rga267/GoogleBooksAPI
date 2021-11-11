cb57c374417f4ac3b5e94b29389ccb404689b32b
Ricardo <rga267@nyu.edu>
2021-11-11 14:59:31 -0500

    added tests and rest of book properties

--- src/app/components/book/book.component.spec.ts
+++ src/app/components/book/book.component.spec.ts
@@ -26,5 +26,25 @@ describe('BookComponent', () => {
   it('shows the title of a book', () => {
     const labelElement = fixture.debugElement.query(By.css('.title'));
     expect(labelElement.nativeElement.textContent).toContain('Title: '+component.book.title);
-  })
+  });
+
+  it('shows the description of a book', () => {
+    const labelElement = fixture.debugElement.query(By.css('.description'));
+    expect(labelElement.nativeElement.textContent).toContain('Description: '+component.book.description);
+  });
+
+  it('shows the type of a book', () => {
+    const labelElement = fixture.debugElement.query(By.css('.type'));
+    expect(labelElement.nativeElement.textContent).toContain('Type: '+component.book.type);
+  });
+
+  it('shows the author of a book', () => {
+    const labelElement = fixture.debugElement.query(By.css('.author'));
+    expect(labelElement.nativeElement.textContent).toContain('Author: '+component.book.author);
+  });
+
+  it('shows the thumbnail of a book', () => {
+    const labelElement = fixture.debugElement.query(By.css('.thumbnail img'));
+    expect(labelElement.nativeElement.getAttribute('src')).toBe(component.book.thumbnail);
+  });
 });
